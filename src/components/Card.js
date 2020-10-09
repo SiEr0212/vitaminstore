7b470f7649d27eb98d316dfbb49d6094b221e603
SiEr0212 <simonertel@gmx.de>
2020-10-09 21:29:43 +0200

    created card component, styled cards

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./Card.css";
+
+const Card = () => (
+  <div className="card">
+    <img src="juice.jpg" alt="Vitamin Juice" className="card-image" />
+    <div className="container">
+      <h3>
+        Vitamin Juice <span className="price">24.99€</span>
+      </h3>
+      <p>
+        Need a jum on your vitamins while drinking? Tired of popping pills?
+        Drink our vitamins enhanced juice, available in several flavours.
+      </p>
+    </div>
+  </div>
+);
+
+export default Card;
