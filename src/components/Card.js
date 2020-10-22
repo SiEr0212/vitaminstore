b221cbb94a12506f203ec9193fcc363b372a37f5
SiEr0212 <simonertel@gmx.de>
2020-10-22 14:47:44 +0200

    working on flip card animation and adding a data.JSON

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,19 +2,26 @@ import React, { Component } from "react";
 import "./Card.css";
 
 const Card = (props) => (
-  <div 
-  className={props.card.animation}// passes the styles from card in card.css and triggers card animated zoomOut on click
-  onClick={() => props.clickCard(props.card)}
+  <div
+    className={props.card.animation} // passes the styles from card in card.css and triggers card animated zoomOut on click
   >
-    <img src="juice.jpg" alt="Vitamin Juice" className="card-image" />
-    <div className="container">
+    <div className="front" onClick={() => props.showBack(props.card)}>
+      <img src="juice.jpg" alt="Vitamin Juice" className="card-image" />
+      <div className="container">
+        <h3>
+          Vitamin Juice <span className="price">24.99€</span>
+        </h3>
+        <p>
+          Need a jum on your vitamins while drinking? Tired of popping pills?
+          Drink our vitamins enhanced juice, available in several flavours.
+        </p>
+      </div>
+    </div>
+    <div className="container-back back" onClick={() => props.showFront(props.card)}>
       <h3>
         Vitamin Juice <span className="price">24.99€</span>
       </h3>
-      <p>
-        Need a jum on your vitamins while drinking? Tired of popping pills?
-        Drink our vitamins enhanced juice, available in several flavours.
-      </p>
+      <p>{props.card.description}</p>
     </div>
   </div>
 );
