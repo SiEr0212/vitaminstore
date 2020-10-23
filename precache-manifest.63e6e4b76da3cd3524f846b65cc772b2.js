fe1dce064652d6644ab596a93d7ed9026cd04cf3
SiEr0212 <simonertel@gmx.de>
2020-10-23 18:36:48 +0200

    Updates

---
+++ precache-manifest.63e6e4b76da3cd3524f846b65cc772b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60bd7f90768c7a88f4b9425cf15cce90",
+    "url": "/vitaminstore/index.html"
+  },
+  {
+    "revision": "8998b95d535e73d1ce8d",
+    "url": "/vitaminstore/static/css/main.422efe03.chunk.css"
+  },
+  {
+    "revision": "0fb8c64583a50c07ddfc",
+    "url": "/vitaminstore/static/js/2.54fa1253.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vitaminstore/static/js/2.54fa1253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8998b95d535e73d1ce8d",
+    "url": "/vitaminstore/static/js/main.43239b35.chunk.js"
+  },
+  {
+    "revision": "b283bb29cb03f7425781",
+    "url": "/vitaminstore/static/js/runtime-main.b468ab06.js"
+  },
+  {
+    "revision": "a68903b343d04239a08a3b24ef70cb24",
+    "url": "/vitaminstore/static/media/logo.a68903b3.png"
+  }
+]);
